9844c779d224d42ce20268c5a003ef3da51143c3
Patrick Awuku <apatrickadjei@gmail.com>
2026-09-23 14:43:42 +0000

    Switch checkout to production URL

--- src/components/DonateButton.tsx
+++ src/components/DonateButton.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from "react";
 
-const CHECKOUT_URL = "https://checkoutuat.itcsrvc.com/transflow-checkout/lic";
+const CHECKOUT_URL = "https://checkout.itcsrvc.com/transflow-checkout/lic";
 
 export default function DonateButton({ className = "" }: { className?: string }) {
   const dialogRef = useRef<HTMLDialogElement>(null);
